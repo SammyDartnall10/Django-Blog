0cc468fc90ddd8614ff6328d4a28c88aec69a9e8
Sammy Dartnall <samantha.megan10@gmail.com>
2019-05-15 21:34:25 +0000

    changed allowed hosts

--- blog/settings.py
+++ blog/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-blog-new-sammydartnall.c9users.io', 'blog-test-app.herokuapp.com']
+ALLOWED_HOSTS = ['django-blog-new-sammydartnall.c9users.io', 'djangoblogtestingapp.herokuapp.com']
 
 
 # Application definition
